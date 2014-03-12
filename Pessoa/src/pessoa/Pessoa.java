800fd325fac55f62998d232b91c40c72e54adc0d
lcskdc <lcskdc@hotmail.com>
2014-03-11 23:02:30 -0300

    Meu terceiro commit

---
+++ Pessoa/src/pessoa/Pessoa.java
@@ -0,0 +1,13 @@
+package pessoa;
+
+/**
+ *
+ * @author Lucas Pacheco Oliveira
+ */
+public class Pessoa {
+
+    public static void main(String[] args) {
+        System.out.println("Pessoa");
+    }
+    
+}
