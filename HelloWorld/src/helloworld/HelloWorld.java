0b86214c7dad6589da8e8f8f5bd7a5ca1396a5c2
lcskdc <lcskdc@hotmail.com>
2014-03-11 22:54:20 -0300

    Meu primeiro commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ *
+ * @author Lucas Pacheco Oliveira
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
